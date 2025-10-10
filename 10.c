64011fe5088ff707c900a4fcf0350829ed605043
Jonathan Debove <jondebove@outlook.com>
2025-10-10 22:48:07 +0200

    Gauss-Siedel + SOR in step 10

--- 10.c
+++ 10.c
@@ -3,12 +3,13 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <assert.h>
 #include <math.h>
 
 #include "utils.h"
 
 /*
- * 2D Laplace equation
+ * 2D Poisson equation
  * d2p/dx2 + d2p/dy2 = b
  *
  * p(i,j) = ((p(i+1,j)+p(i-1,j))* dy2 + (p(i,j+1)+p(i,j-1))* dx2 - dx2*dy2*b(i,j)) / (2*(dx2+dy2))
@@ -20,64 +21,84 @@
  * b(0.5,0.25)=100, b(1.5,0.75)=-100, b=0 elsewhere
  */
 
+static
+double poisson_iter(struct mat p, struct mat b,
+		double dx2, double dy2, double omega)
+{
+	assert(p.nrow > 0);
+	assert(p.ncol > 0);
+	assert(p.nrow == b.nrow);
+	assert(p.ncol == b.ncol);
+	assert(dx2 > 0);
+	assert(dy2 > 0);
+
+	double den = (dx2 + dy2) * 2.0;
+	double dxdy2 = dx2 * dy2 / den;
+	dx2 /= den;
+	dy2 /= den;
+
+	int i, j, k;
+	double epsmax = 0.0;
+	/* Red-black sweep */
+	for (k = 0; k <= 1; k++) {
+		for (j = 1; j < p.ncol - 1; j++) {
+			for (i = 2 - (j + k) % 2; i < p.nrow - 1; i += 2) {
+				double eps = omega * (
+						(*mat_at(p,i+1,j) +
+						 *mat_at(p,i-1,j)) * dy2 -
+						(*mat_at(p,i,j)) +
+						(*mat_at(p,i,j+1) +
+						 *mat_at(p,i,j-1)) * dx2 -
+						(*mat_at(b,i,j)) * dxdy2);
+				double p1 = *mat_at(p, i, j) + eps;
+
+				eps /= MAX(ABS(p1), ABS(*mat_at(p, i, j)));
+				epsmax = MAX(epsmax, eps);
+
+				*mat_at(p, i, j) = p1;
+			}
+		}
+	}
+	return epsmax;
+}
+
 int main(void)
 {
 	int nx = 20;
 	int ny = 10;
 	int nt = 500;
-	double dx = 2.0 / (nx - 1);
-	double dy = 1.0 / (ny - 1);
 
-	int n = 0;
-	int i = 0;
-	int j = 0;
+	double dx2 = 2.0 / (nx - 1);
+	double dy2 = 1.0 / (ny - 1);
+	dx2 *= dx2;
+	dy2 *= dy2;
 
-	struct mat p0, p1, b;
-	mat_create(&p0, nx, ny);
-	mat_create(&p1, nx, ny);
+	struct mat p, b;
+	mat_create(&p, nx, ny);
 	mat_create(&b, nx, ny);
 
 	*mat_at(b, (1*nx)/4, (1*ny)/4) = +100.0;
 	*mat_at(b, (3*nx)/4, (3*ny)/4) = -100.0;
 
-	mat_print(p0, "p", 0);
+	mat_print(p, "p", 0);
 
-	double dx2 = dx * dx;
-	double dy2 = dy * dy;
-	double den = (dx2 + dy2) * 2.0;
+	// SOR
+	double omega = 1.0;
+	double rho = (dy2 * cos(M_PI / p.nrow) +
+			dx2 * cos(M_PI / p.ncol)) / (dx2 + dy2);
+	rho *= rho;
+
+	int n;
 	for (n = 1; n <= nt; n++) {
-		// Equation
-		for (j = 1; j < ny - 1; j++) {
-			for (i = 1; i < nx - 1; i++) {
-				*mat_at(p1, i, j) =
-					(dy2*(*mat_at(p0,i+1,j) +
-					      *mat_at(p0,i-1,j)) +
-					 dx2*(*mat_at(p0,i,j+1) +
-					      *mat_at(p0,i,j-1)) -
-					 dx2*dy2*(*mat_at(b,i,j))) / den;
-			}
-		}
-		// BC
-		for (i = 0; i < nx; i++) {
-			*mat_at(p1, i, 0) = 0.0;
-			*mat_at(p1, i, ny-1) = 0.0;
-		}
-		for (j = 0; j < ny; j++) {
-			*mat_at(p1, 0, j) = 0.0;
-			*mat_at(p1, nx-1, j) = 0.0;
-		}
-
-		if (mat_dist(p0, p1) < 1e-6) {
-			mat_print(p1, "p", n);
-			break;
-		}
-		if (n % 100 == 0) mat_print(p1, "p", n);
-
-		SWAP(struct mat, p0, p1);
+		double eps = poisson_iter(p, b, dx2, dy2, omega);
+		mat_print(p, "p", n);
+		if (eps < 1e-8) break;
+		omega = n == 1 ? 2.0 / (2.0 - rho) : 4.0 / (4.0 - rho*omega);
 	}
+	WARNF("n=%d\n", n);
 
-	mat_destroy(&p0);
-	mat_destroy(&p1);
+	mat_destroy(&p);
+	mat_destroy(&b);
 
 	return 0;
 }
